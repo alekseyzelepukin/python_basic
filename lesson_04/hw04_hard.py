00db530a4681c9716702f43de5bc2833831a187c
Aleksey Zelepukin <aleksey.zelepukin@gmail.com>
2019-04-29 14:44:57 +0300

    Update hw04_hard.py

--- lesson_04/hw04_hard.py
+++ lesson_04/hw04_hard.py
@@ -74,10 +74,10 @@ for i, n in enumerate(num, start=1):
 
 print(f'Произведение: {smax}, индекс смещения: {idx}')
 
-# проверка:
-# num[idx:idx+5]
+# # проверка:
+# print(num[idx:idx+5])
 # s = 1
-# for i in num[365:365 + 5]:
+# for i in num[idx:idx+5]:
 #     s *= int(i)
 # print(s)
 
