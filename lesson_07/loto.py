5e7f29426dfc2e04651e2af23c066b733ba50857
Aleksey Zelepukin <aleksey.zelepukin@gmail.com>
2019-05-15 16:25:42 +0300

    Update loto.py

--- lesson_07/loto.py
+++ lesson_07/loto.py
@@ -42,3 +42,91 @@
 Подсказка: для работы с псевдослучайными числами удобно использовать 
 модуль random: http://docs.python.org/3/library/random.html
 """
+
+import random, copy
+
+MAX_BARREL = 90
+DIGITS_IN_CARD = 15
+DIGITS_IN_LINE = 5
+
+
+def gen_card():
+    # combination of 15 random digits 1..MAX_BARREL without repetitions
+    num_comb = random.sample(range(1, MAX_BARREL + 1), DIGITS_IN_CARD)
+    # make list of sorted slices of num_comb of size DIGITS_IN_LINE;
+    # ie. divide num_comb into 3 sorted lists
+    card = [sorted(num_comb[i:i + DIGITS_IN_LINE]) for i in range(0, len(num_comb), DIGITS_IN_LINE)]
+    return card
+
+
+def gen_barr_list():
+    return random.sample(range(1, MAX_BARREL + 1), 90)
+
+
+def get_barrel(barr_list):
+    return barr_list.pop()
+
+
+def show_card(card):
+    card = copy.deepcopy(card)  # we don't want to modify the original list
+    placeholders = ' '.join(['{:>2}' for i in range(9)])  # create 9 placeholders in 'cells', separated by spaces
+    for line in card:
+        for space in ' ' * 4:
+            line.insert(random.randint(0, len(line) - 1), space)  # randomly insert 4 spaces in each card line
+    return [placeholders.format(*line) for line in card]
+
+
+def update_card(card, barrel):
+    # return [['-' if x == barrel else x for x in line] for line in card]
+    for line in card:
+        yield ['-' if x == barrel else x for x in line]
+
+
+def is_empty(card):
+    for line in card:
+        for elt in line:
+            if elt != '-':
+                return False
+    return True
+
+
+def barr_in_card(card, barrel):
+    return barrel in [barrel for line in card for barrel in line]
+
+
+# return True
+
+
+def play_round():
+    print('''Welcome to the loto game. You are playing against the computer.
+You must not mistake or you lose.
+Generating your card and mixing the barrels...\n''')
+    player_card, comp_card = gen_card(), gen_card()
+    barrels = gen_barr_list()
+    while True:  # check if all digits are crossed
+        next_barrel = get_barrel(barrels)
+        print('New barrel: {}. Left: {}'.format(next_barrel, len(barrels)))
+        print("{0} Player's card {0}\n{1}\n{2}\n{3}".format('-' * 6, *show_card(player_card)))
+        print("{0} Computer's card {0}\n{1}\n{2}\n{3}".format('-' * 5, *show_card(comp_card)))
+        answ = 'a'
+        while answ not in 'ynq':
+            answ = str(input("Is the barrel in player's card? y/n or q for exit: "))
+        if answ == 'q':
+            break
+        elif (answ == 'y' and barr_in_card(player_card, next_barrel)) or (answ == 'n' and not barr_in_card(player_card,
+                                                                                                           next_barrel)):
+            print("You're right! \n\nNext turn...")
+        else:
+            print("You lose!")
+            break
+        player_card = list(update_card(player_card, next_barrel))
+        comp_card = list(update_card(comp_card, next_barrel))
+        if is_empty(player_card):
+            print('You filled the entire card!')
+            break
+        if is_empty(comp_card):
+            print('Computer filled the entire card!')
+            break
+
+
+play_round()
